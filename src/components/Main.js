c08932b4a7c20de650ff0736d03f9166acefbfb9
Anjana Puri <anjana@i-motor.com.au>
2023-07-23 17:05:53 +1000

    photowall - update images and css

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,18 +10,22 @@ constructor(){
     this.state = {
         posts: [{
             id: 0,
-            description: "beautiful landscape",
-            imageLink: "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
-            "3919321_1443393332_n.jpg"
+            description: "Urban X 18",
+            imageLink: "https://imotor-cms-uploads.s3.ap-southeast-2.amazonaws.com/gJHoa6jJtDWngqu4PvQESH12"
             }, {
             id: 1,
-            description: "Aliens???",
-            imageLink: "https://s3.india.com/wp-content/uploads/2017/12/rocket.jpg"
+            description: "Urban X  Plus 18",
+            imageLink: "https://imotor-cms-uploads.s3.ap-southeast-2.amazonaws.com/5ufaT6hURE6SHjJ1UQ9uY2LY"
             }, {
             id: 2,
-            description: "On a vacation!",
-            imageLink: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg"
-            }]
+            description: "Urban X.",
+            imageLink: "https://imotor-cms-uploads.s3.ap-southeast-2.amazonaws.com/cVzSp2fTFnTfWSocTr2YvHvX"
+            }, {
+            id: 3,
+            description: "Urban X Plus.",
+            imageLink: "https://imotor-cms-uploads.s3.ap-southeast-2.amazonaws.com/u3q3sYBsBkP5y5UpCGDce62U"
+            }
+        ]
     }
 
 this.removePhoto = this.removePhoto.bind(this);
